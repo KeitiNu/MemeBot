214da3b849ebf64a9e245e0b623c71e3f3f3b5c5
KeitiN <keiti.nurmbek@gmail.com>
2022-06-18 20:39:06 +0300

    MemeBot working example without user input

--- main.py
+++ main.py
@@ -1,92 +1,179 @@
-# import discord
-# import os
-# import random
+
 # from dotenv import load_dotenv
 
 
 # load_dotenv()
  
-# client = discord.Bot()
 # token = os.getenv('TOKEN')
 
 
-# @client.event
-# async def on_ready():
-#     print("Logged in as a bot {0.user}".format(client))
-
-
-# @client.event
-# async def on_message(message):
-# 	username = str(message.author).split("#")[0]
-# 	channel = str(message.channel.name)
-# 	user_message = str(message.content)
-
-# 	print(f'Message {user_message} by {username} on {channel}')
-
-# 	if message.author == client.user:
-# 		return
-
-# 	if channel == "random":
-# 		if user_message.lower() == "hello" or user_message.lower() == "hi":
-# 			await message.channel.send(f'Hello {username}')
-# 			return
-# 		elif user_message.lower() == "bye":
-# 			await message.channel.send(f'Bye {username}')
-# 		elif user_message.lower() == "tell me a joke":
-# 			jokes = [" Can someone please shed more\
-# 			light on how my lamp got stolen?",
-# 					"Why is she called llene? She\
-# 					stands on equal legs.",
-# 					"What do you call a gazelle in a \
-# 					lions territory? Denzel."]
-# 			await message.channel.send(random.choice(jokes))
-
-# client.run(token)
-
-# bot.py
 # bot.py
 import os
-import random
-
+import io
 import discord
-from dotenv import load_dotenv
+import nest_asyncio 
+nest_asyncio.apply()
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = 'OTg3NjQ0ODcwMjc1NTIyNTcw.Gjb0hM.N9J2pg1N3KnSWZS2lC56v2TF9T_rN2rXbmqdok'
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
+
+# Model references
+
+# dalle-mega
+# DALLE_MODEL = "dalle-mini/dalle-mini/mega-1:latest"  # can be wandb artifact or 🤗 Hub or local folder or google bucket
+DALLE_COMMIT_ID = None
+
+# if the notebook crashes too often you can use dalle-mini instead by uncommenting below line
+DALLE_MODEL = "dalle-mini/dalle-mini/mini-1:v0"
+
+# VQGAN model
+VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
+VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
+import jax
+import jax.numpy as jnp
+
+# check how many devices are available
+jax.local_device_count()
+
+print(f'has checked awailable devices!')
+
+# Load models & tokenizer
+from dalle_mini import DalleBart, DalleBartProcessor
+from vqgan_jax.modeling_flax_vqgan import VQModel
+from transformers import CLIPProcessor, FlaxCLIPModel
+
+# Load dalle-mini
+model, params = DalleBart.from_pretrained(
+    DALLE_MODEL, revision=DALLE_COMMIT_ID, dtype=jnp.float16, _do_init=False
+)
+
+# Load VQGAN
+vqgan, vqgan_params = VQModel.from_pretrained(
+    VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False
+)
+
+from flax.jax_utils import replicate
+
+params = replicate(params)
+vqgan_params = replicate(vqgan_params)
+
+from functools import partial
+
+# model inference
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
+def p_generate(
+    tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
+):
+    return model.generate(
+        **tokenized_prompt,
+        prng_key=key,
+        params=params,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        condition_scale=condition_scale,
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+# decode image
+@partial(jax.pmap, axis_name="batch")
+def p_decode(indices, params):
+    return vqgan.decode_code(indices, params=params)
+
+
+import random
+
+# create a random key
+seed = random.randint(0, 2**32 - 1)
+key = jax.random.PRNGKey(seed)
+
+from dalle_mini import DalleBartProcessor
+
+processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+
+
+
+prompts = [
+    "the Eiffel tower",
+]
+
+
+
+tokenized_prompts = processor(prompts)
+
+tokenized_prompt = replicate(tokenized_prompts)
+
+
+
+
+# number of predictions per prompt
+n_predictions = 1
+
+# We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
+gen_top_k = None
+gen_top_p = None
+temperature = None
+cond_scale = 10.0
+
+
+from flax.training.common_utils import shard_prng_key
+import numpy as np
+from PIL import Image
+from tqdm.notebook import trange
+
+print(f"Prompts: {prompts}\n")
+# generate images
+images = []
+for i in trange(max(n_predictions // jax.device_count(), 1)):
+    # get a new key
+    key, subkey = jax.random.split(key)
+    # generate images
+    encoded_images = p_generate(
+        tokenized_prompt,
+        shard_prng_key(subkey),
+        params,
+        gen_top_k,
+        gen_top_p,
+        temperature,
+        cond_scale,
+    )
+
+
+    # remove BOS
+    encoded_images = encoded_images.sequences[..., 1:]
+    # decode images
+    from IPython.display import display
+
+    decoded_images = p_decode(encoded_images, vqgan_params)
+    decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+    for decoded_img in decoded_images:
+        img = Image.fromarray(np.asarray(decoded_img * 255, dtype=np.uint8))
+        images.append(img)
+        display(img)
+
+        print()
+
+
+
+    @client.event
+    async def on_message(message):
+      if message.author == client.user:
         return
 
-#     fullstring = "StackAbuse"
-# substring = "tack"
+      if '#BotMeme' in message.content:
+        # response = discord.File(open(img))
+        
+        arr = io.BytesIO()
+        img.save(arr, format='PNG')
+        arr.seek(0)
+        file = discord.File(fp=arr, filename='image.png')
+        await message.channel.send(file = file)
 
-# if substring in fullstring:
-#     print("Found!")
-# else:
-#     print("Not found!")
-
-
-
-    bot_Greeting = 'A meme for you. Ha-ha'
-    
-
-    if '#BotMeme' in message.content:
-        response = bot_Greeting
-        await message.channel.send(response)
 
 client.run(TOKEN)
